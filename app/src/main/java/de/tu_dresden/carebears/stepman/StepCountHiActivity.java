b49bfde0aa6f322f4fa36e7fc7469a179508117b
Jan Stephan <jan.stephan.dd@gmail.com>
2015-12-09 16:47:10 +0100

    Added some layout files

--- app/src/main/java/de/tu_dresden/carebears/stepman/StepCountHiActivity.java
+++ app/src/main/java/de/tu_dresden/carebears/stepman/StepCountHiActivity.java
@@ -3,11 +3,11 @@ package de.tu_dresden.carebears.stepman;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class StepCountHiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_step_count_hi);
     }
 }
---
+++ app/src/main/java/de/tu_dresden/carebears/stepman/StepWidthActivity.java
@@ -0,0 +1,13 @@
+package de.tu_dresden.carebears.stepman;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StepWidthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_width);
+    }
+}
