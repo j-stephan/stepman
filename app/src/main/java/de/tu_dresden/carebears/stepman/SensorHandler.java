a825bc836ec8604e50d6958f15336e42264a2a1b
Lukas Resch <github@lukasresch.com>
2015-12-12 18:47:21 +0100

    added SensorHandler interface

---
+++ app/src/main/java/de/tu_dresden/carebears/stepman/SensorHandler.java
@@ -0,0 +1,16 @@
+package de.tu_dresden.carebears.stepman;
+
+/**
+ * Created by frog on 12.12.15.
+ */
+public interface SensorHandler {
+
+    public boolean initialize();
+    public boolean isInitialized();
+    public void close();
+    public void reset();
+
+    public String getStatusMessage();
+    public int getData();
+
+}
